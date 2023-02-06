4f08951258ebaf8f14d18c929e265441e9d0f10a
Vadim_Vinogradov <voldo90@inbox.ru>
2023-02-06 22:50:14 +0300

    Пустой проект

---
+++ src/PreProjecktVaduha/Main.java
@@ -0,0 +1,4 @@
+package PreProjecktVaduha;
+
+public class Main {
+}
---
+++ src/TestingWork/CalcException.java
@@ -0,0 +1,7 @@
+package TestingWork;
+
+public class CalcException extends Exception {
+    public CalcException (String wrongInput){
+        super (wrongInput);
+    }
+}
